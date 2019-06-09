ed88e38ace1edcabaf2b2890e93bac16a2bf6287
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-06-09 17:42:16 -0500

    fetch and parse data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  
+constructor(props){
+  super(props);
+
+  this.state = {
+    isLoading: true,
+    contacts: []
+  };
+}
+
+componentDidMount(){
+  this.fetchData();
+}
+
+fetchData = () => {
+fetch('https://randomuser.me/api/?results=10')
+.then( response => response.json())
+.then(data => console.log(data))
+}
+
   render(){
   return (
     <div className="App">
