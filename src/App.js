c9aa7bde9541c62b8b2f93fcc016bcc50f1bedf6
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-06-09 20:03:06 -0500

    complete fetch react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -19,18 +18,37 @@ componentDidMount(){
 fetchData = () => {
 fetch('https://randomuser.me/api/?results=10')
 .then( response => response.json())
-.then(data => console.log(data))
+.then(data => data.results.map(user => ({
+  name: `${user.name.first}`,
+  address:`${user.location.street}, ${user.location.city}`,
+  email: `${user.email}`
+}
+
+)))
+.then(contacts => this.setState({
+  contacts,
+  isLoading: false
+}))
+
+
+
+.catch(error => console.log("parsing failed...", error))
 }
 
   render(){
+    // const{isLoading, contacts} = this.state;
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
-  }
-}
+      <div className="App">
+        <div>   
+       {
+            !this.state.isLoading && this.state.contacts.length > 0 ? this.state.contacts.map(contact => {
+             return <p key={contact.name}><br/>{contact.name}<br/>{contact.address}<br/>{contact.email}</p>
+            }) : null
 
+          }
+        </div>
+        </div>
+
+  )}
+        }
 export default App;
